b3fe2bf5a29912962af2d537e171139070b32743
chowiekomba <chowiekomba@gmail.com>
2026-04-27 11:43:02 -0600

    update to 26.1.x

--- src/main/java/chowie/abilitytools/mixin/LivingEntityMixin.java
+++ src/main/java/chowie/abilitytools/mixin/LivingEntityMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
