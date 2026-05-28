557a62db25ad6612c960b94152b6b0097bd89c0f
chowiekomba <chowiekomba@gmail.com>
2026-05-27 21:36:47 -0600

    roid rage gun balancing

--- src/main/java/chowie/abilitytools/item/custom/misc/RoidRageGunItem.java
+++ src/main/java/chowie/abilitytools/item/custom/misc/RoidRageGunItem.java
@@ -3,6 +3,7 @@ package chowie.abilitytools.item.custom.misc;
 import chowie.abilitytools.util.ModSounds;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -16,22 +17,23 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.component.UseCooldown;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class RoidRageGunItem extends ProjectileWeaponItem {
-    private final UseCooldown cooldown = new UseCooldown(1.0F);
-    float knockbackAmount = 0.5f;
+    private final UseCooldown cooldown = new UseCooldown(0.1F);
+    float knockbackAmount = 0.2f;
 
     public RoidRageGunItem(Properties properties) {
         super(properties);
     }
 
     @Override
-    public Predicate<ItemStack> getAllSupportedProjectiles() {
-        return null;
+    public @NonNull Predicate<ItemStack> getAllSupportedProjectiles() {
+        return itemStack -> itemStack.is(ItemTags.COPPER_GOLEM_STATUES);
     }
 
     @Override
@@ -40,20 +42,21 @@ public class RoidRageGunItem extends ProjectileWeaponItem {
     }
 
     @Override
-    public void appendHoverText(ItemStack itemStack, TooltipContext context, TooltipDisplay display, Consumer<Component> builder, TooltipFlag tooltipFlag) {
+    public void appendHoverText(@NonNull ItemStack itemStack, @NonNull TooltipContext context, @NonNull TooltipDisplay display,
+                                Consumer<Component> builder, @NonNull TooltipFlag tooltipFlag) {
         builder.accept(Component.translatable("item.ability-tools.roid_rage_gun.tooltip"));
         super.appendHoverText(itemStack, context, display, builder, tooltipFlag);
     }
 
     @Override
-    protected void shootProjectile(LivingEntity shooter, Projectile projectileEntity, int index, float power,
+    protected void shootProjectile(@NonNull LivingEntity shooter, Projectile projectileEntity, int index, float power,
                                    float uncertainty, float angle, @Nullable LivingEntity targetOverride) {
         projectileEntity.shootFromRotation(shooter, shooter.getXRot(), shooter.getYRot() + angle, 0.0F, power, uncertainty);
         shooter.level().addFreshEntity(projectileEntity);
     }
 
     @Override
-    public InteractionResult use(Level level, Player player, InteractionHand hand) {
+    public @NonNull InteractionResult use(@NonNull Level level, Player player, @NonNull InteractionHand hand) {
         double x = player.getX() + player.getLookAngle().x;
         double z = player.getZ() + player.getLookAngle().z;
 
@@ -66,7 +69,7 @@ public class RoidRageGunItem extends ProjectileWeaponItem {
 
         level.playSound(null, player.getOnPos(), ModSounds.GUNSHOT, SoundSource.PLAYERS);
 
-        // cooldown.apply(player.getItemInHand(hand), player);
+        cooldown.apply(player.getItemInHand(hand), player);
 
         return super.use(level, player, hand);
     }
